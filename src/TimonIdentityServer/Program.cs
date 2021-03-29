a3850d53755b84383862a9eb1a6438cbd729f10f
jjchiw <jjchiw@gmail.com>
2021-03-29 23:47:50 +0200

    log connectionstring

--- src/TimonIdentityServer/Program.cs
+++ src/TimonIdentityServer/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -7,6 +8,8 @@ namespace TimonIdentityServer
     {
         public static void Main(string[] args)
         {
+            var x = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+            System.Console.WriteLine(x);
             CreateHostBuilder(args).Build().Run();
         }
 
