99deec526172eaa419f3d4c9ef7957e4dc0971b2
jjchiw <jjchiw@gmail.com>
2021-03-29 22:05:50 +0200

    Add password to pfx

--- src/TimonIdentityServer/build-push.sh
+++ src/TimonIdentityServer/build-push.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker build . -t jjchiw/timon-identity-server:$(git rev-parse --short HEAD)
-docker push jjchiw/timon-identity-server:$(git rev-parse --short HEAD)
-git rev-parse --short HEAD
+# docker push jjchiw/timon-identity-server:$(git rev-parse --short HEAD)
+# git rev-parse --short HEAD
